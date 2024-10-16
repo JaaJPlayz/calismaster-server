02350214d8c46dd9022512b8a29d40a449c6c6c1
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2024-10-16 09:02:29 -0300

    Fix: Fixing lint issues and database path

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { Request, Response } from "express";
 import { PORT } from "./constants";
 
+import workoutRouter from "./routers/workout.router";
+
 const app = express();
 
 app.get("/", (_req: Request, res: Response) => {
   res.send("Hello World!");
 });
+
+app.use("/api/workouts", workoutRouter);
 
 app.listen(PORT, () => {
   console.log("Example app listening on port 3000!");
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,4 +1,4 @@
-const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('./database.db')
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./src/database/database.db");
 
-export default db
+export default db;
